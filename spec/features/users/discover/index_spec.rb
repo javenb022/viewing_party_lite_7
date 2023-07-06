0b663b5a37b0033e7eec94f32f8f8023949e5e60
Lauren Golden <llgolden@gmail.com>
2023-07-06 16:57:09 -0600

    US7 test setup

---
+++ spec/features/users/discover/index_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe "Discover Index Page" do
+  before :each do
+    @user_1 = User.create!(name: "Simon", email: "simon@gmail.com")
+
+    visit "/users/#{@user_1.id}/discover"
+  end
+
+  it "should display ways to discover movies" do
+    expect(page).to have_button("Discover Top Rated Movies")
+    expect(page).to have_field("Search by Movie Title")
+    expect(page).to have_button("Search")
+  end
+
+  xit "should take the user to the movies results page from the Discover Top Movies button" do 
+    click_button "Discover Top Rated Movies"
+    expect(page).to have_current_path("/users/#{@user_1.id}/movies")
+  end
+
+  xit "should take the user to the movies results page from the Search button" do
+    click_button "Search"
+    expect(page).to have_current_path("/users/#{@user_1.id}/movies")
+  end
+end
