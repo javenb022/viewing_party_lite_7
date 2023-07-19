c3ceae80a245ebbf8b6b08dfceea0358669d791d
Lauren Golden <llgolden@gmail.com>
2023-07-18 21:14:27 -0600

    US in progress

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 describe "admin dashboard" do
   before(:each) do
     admin = User.create(name: "Lauren", email: "lauren@gmail.com", password: "passw0rd", role: 2)
-    @user_1 = create(:user)
-    @user_2 = create(:user)
-    @user_3 = create(:user)
+    @user1 = create(:user)
+    @user2 = create(:user)
+    @user3 = create(:user)
     visit login_path
 
     fill_in "Email", with: "lauren@gmail.com"
@@ -15,12 +15,13 @@ describe "admin dashboard" do
 
   it "has a list of default users email addresses" do
     expect(current_path).to eq(admin_dashboard_path)
+
     expect(page).to have_content(@user1.email)
     expect(page).to have_content(@user2.email)
     expect(page).to have_content(@user3.email)
   end
 
-  xit "takes me to the admin users dashboard when I click an email address and I see the same dashboard that user would see" do
+  it "takes me to the admin users dashboard when I click an email address and I see the same dashboard that user would see" do
     click_link "#{@user1.email}"
     expect(page).to have_content("#{@user1.name}'s Dashboard")
     expect(page).to have_button("Discover Movies")
